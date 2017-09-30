68b39b5ff6b5f3491c71a1276f228f2c4e94deba
Xavier <shavy_00@hotmail.com>
2017-09-29 22:16:38 -0600

    Agregando app.js

---
+++ angular/app.js
@@ -0,0 +1,18 @@
+//var app = angular.module('myApp', []);
+var app = angular.module('myApp', ['ngRoute']); 
+
+
+app.controller('mainCtrl', ['$scope', function($scope) {
+
+$scope.test = "test";
+
+
+
+
+
+
+}]);
+
+
+
+
